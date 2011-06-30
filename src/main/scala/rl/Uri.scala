fce2831f9d5160b5c3a9c34455a0dd5d04b04d5b
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-06-30 11:37:09 +0100

    Adds 400 IP6 address test cases

--- src/main/scala/rl/Uri.scala
+++ src/main/scala/rl/Uri.scala
@@ -103,12 +103,61 @@ object Uri {
     ("""(?iu)^(((?=(?>.*?::)(?!.*::)))(::)?([0-9A-F]{1,4}::?){0,5}|([0-9A-F]{1,4}:){6})(\2([0-9A-F]{1,4}(::?|$)){0,2}""" +
         """|((25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.|$)){4}|[0-9A-F]{1,4}:[0-9A-F]{1,4})(?<![^:]:)(?<!\.)\z""").r
 
+  trait IPv6AddressParser extends RegexParsers {
+    def hexDigit = hexDigits
+    def decOctet = """25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d""".r
+    def dottedDecOctet = decOctet <~ "."
+
+    private def IPv4Address = dottedDecOctet ~ dottedDecOctet ~ dottedDecOctet ~ decOctet ^^ {
+      case a ~ b ~ c ~ d => a + "." + b + "." + c + "." + d
+    }
+
+
+    private def h16_2 = repN(2, hexDigit)
+    private def h16_3 = repN(3, hexDigit)
+    private def h16_4 = repN(4, hexDigit)
+    private def h16_multi = (h16_4 | h16_3 | h16_2) ^^ { _ mkString "" }
+    private def h16 = h16_multi | hexDigit
+
+    private def h16Colon = h16 ~ ":" ^^ { case a ~ b => a + b }
+    private def h16Colon_2 = repN(2, h16Colon)
+    private def h16Colon_3 = repN(3, h16Colon)
+    private def h16Colon_4 = repN(4, h16Colon)
+    private def h16Colon_5 = repN(5, h16Colon)
+    private def h16Colon_6 = repN(6, h16Colon)
+    private def h16ColonN(max: Int) = max match {
+      case 6 => ((h16Colon_6 | h16Colon_5 | h16Colon_4| h16Colon_3 | h16Colon_2) ^^ { _ mkString "" }) | h16Colon
+      case 5 => ((h16Colon_5 | h16Colon_4| h16Colon_3 | h16Colon_2) ^^ { _ mkString "" }) | h16Colon
+      case 4 => ((h16Colon_4 | h16Colon_3 | h16Colon_2) ^^ { _ mkString "" }) | h16Colon
+      case 3 => ((h16Colon_3 | h16Colon_2) ^^ { _ mkString "" }) | h16Colon
+      case 2 => (h16Colon_2 ^^ { _ mkString "" }) | h16Colon
+      case 1 => h16Colon
+    }
+    private def h16Colonh16N(max: Int) = h16ColonN(max) ~ h16 ^^ { case a ~ b => a + b }
+    private def nH16Colon(n: Int) = repN(n, h16Colon) ^^ { _ mkString "" }
+
+    private def flatOpt(parser: => Parser[String]): Parser[String] = opt(parser) ^^ { _ getOrElse  ""}
+
+    private def ls32 = (h16Colon ~ h16 ^^ { case a ~ b => a + b } ) | IPv4Address
+
+    private def ip6_1 = nH16Colon(6) ~ ls32 ^^ { case a ~ b => a + b }
+    private def ip6_2 = "::" ~ nH16Colon(6) ~ ls32 ^^ { case a ~ b ~ c => a + b + c }
+    private def ip6_3 = flatOpt(h16) ~ "::" ~ nH16Colon(4) ~ ls32 ^^ { case a ~ b ~ c ~ d => a + b + c + d }
+    private def ip6_4 = flatOpt(h16Colonh16N(1)) ~ "::" ~ nH16Colon(3) ~ ls32 ^^ { case a ~ b ~ c ~ d => a + b + c + d }
+    private def ip6_5 = flatOpt(h16Colonh16N(2)) ~ "::" ~ nH16Colon(2) ~ ls32 ^^ { case a ~ b ~ c ~ d => a + b + c + d }
+    private def ip6_6 = flatOpt(h16Colonh16N(3)) ~ "::" ~ nH16Colon(1) ~ ls32 ^^ { case a ~ b ~ c ~ d => a + b + c + d }
+    private def ip6_7 = flatOpt(h16Colonh16N(4)) ~ "::" ~ ls32 ^^ { case a ~ b ~ c  => a + b + c }
+    private def ip6_8 = flatOpt(h16Colonh16N(5)) ~ "::" ~ h16 ^^ { case a ~ b ~ c => a + b + c }
+    private def ip6_9 = flatOpt(h16Colonh16N(6)) ~ "::" ^^ { case a ~ b => a + b }
+    def IPv6Address = (ip6_9 | ip6_8 | ip6_7 | ip6_6 | ip6_5 | ip6_4 | ip6_3 | ip6_2 | ip6_1)
+
+  }
 
   /**
    * Implements a parser for the ABNF found in appendix A of RFC-3986
    * Source: http://tools.ietf.org/html/rfc3986#appendix-A
    */
-  private[rl] trait UriParser extends RegexParsers {
+  private[rl] trait UriParser extends IPv6AddressParser {
 
     def subDelims = subDelimChars
     def genDelims = genDelimChars
@@ -117,7 +166,7 @@ object Uri {
     def digit = """\d""".r
 
     def unreserved = alpha | digit | "-" | "." | "_" | "~"
-    def hexDigit = hexDigits
+
     def pctEncoded = "%" ~> hexDigit ~ hexDigit ^^ { case a ~ b => a + b }
     def pchar = unreserved | pctEncoded | subDelims | ":" | "@"
 
@@ -135,7 +184,6 @@ object Uri {
     }
     def fragmentOpt = opt("#" ~> fragment) ^^ { _ flatMap { a => a } }
 
-
     def pathSegments = rep("/" ~ segment) ^^ { _ mkString "" }
     def pathRootless = segmentNz ~ pathSegments ^^ { case a ~ b => a + b }
     def pathNoScheme = segmentNzNc ~ pathSegments ^^ { case a ~ b => a + b }
@@ -144,10 +192,7 @@ object Uri {
     def pathAbEmpty = rep("/" ~ segment) ^^ { _ mkString "" }
     def path = pathAbEmpty | pathAbsolute | pathNoScheme | pathRootless
 
-    def regName = rep(unreserved | pctEncoded | subDelims) ^^ { _ mkString "" }
-
-    def decOctet = """25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d""".r
-    def dottedDecOctet = decOctet <~ "."
+    def regName = rep(unreserved | pctEncoded | subDelims) ^^ { h => HostNode(h mkString "") }
 
     def ipv4Address = dottedDecOctet ~ dottedDecOctet ~ dottedDecOctet ~ decOctet ^^ {
       case a ~ b ~ c ~ d => {
@@ -155,56 +200,23 @@ object Uri {
       }
     }
 
-    def h16_2 = repN(2, hexDigit)
-    def h16_3 = repN(3, hexDigit)
-    def h16_4 = repN(4, hexDigit)
-    def h16_multi = (h16_4 | h16_3 | h16_2) ^^ { _ mkString "" }
-    def h16 = h16_multi | hexDigit
 
-    def h16Colon = h16 ~ ":" ^^ { case a ~ b => a + b }
-    def h16Colon_2 = repN(2, h16Colon)
-    def h16Colon_3 = repN(3, h16Colon)
-    def h16Colon_4 = repN(4, h16Colon)
-    def h16Colon_5 = repN(5, h16Colon)
-    def h16Colon_6 = repN(6, h16Colon)
-    def h16ColonN(max: Int) = max match {
-      case 6 => ((h16Colon_6 | h16Colon_5 | h16Colon_4| h16Colon_3 | h16Colon_2) ^^ { _ mkString "" }) | h16Colon
-      case 5 => ((h16Colon_5 | h16Colon_4| h16Colon_3 | h16Colon_2) ^^ { _ mkString "" }) | h16Colon
-      case 4 => ((h16Colon_4 | h16Colon_3 | h16Colon_2) ^^ { _ mkString "" }) | h16Colon
-      case 3 => ((h16Colon_3 | h16Colon_2) ^^ { _ mkString "" }) | h16Colon
-      case 2 => (h16Colon_2 ^^ { _ mkString "" }) | h16Colon
-      case 1 => h16Colon
-    }
-    def h16Colonh16N(max: Int) = h16ColonN(max) ~ h16 ^^ { case a ~ b => a + b }
-    def nH16Colon(n: Int) = repN(n, h16Colon) ^^ { _ mkString "" }
-
-    def flatOpt(parser: => Parser[String]): Parser[String] = opt(parser) ^^ { _ getOrElse  ""}
-
-    def ls32 = (h16Colon ~ h16 ^^ { case a ~ b => a + b } ) | ipv4Address
-
-    def ip6_1 = nH16Colon(6) ~ ls32 ^^ { case a ~ b => a + b }
-    def ip6_2 = "::" ~ nH16Colon(6) ~ ls32 ^^ { case a ~ b ~ c => a + b + c }
-    def ip6_3 = flatOpt(h16) ~ "::" ~ nH16Colon(4) ~ ls32 ^^ { case a ~ b ~ c ~ d => a + b + c + d }
-    def ip6_4 = flatOpt(h16Colonh16N(1)) ~ "::" ~ nH16Colon(3) ~ ls32 ^^ { case a ~ b ~ c ~ d => a + b + c + d }
-    def ip6_5 = flatOpt(h16Colonh16N(2)) ~ "::" ~ nH16Colon(2) ~ ls32 ^^ { case a ~ b ~ c ~ d => a + b + c + d }
-    def ip6_6 = flatOpt(h16Colonh16N(3)) ~ "::" ~ nH16Colon(1) ~ ls32 ^^ { case a ~ b ~ c ~ d => a + b + c + d }
-    def ip6_7 = flatOpt(h16Colonh16N(4)) ~ "::" ~ ls32 ^^ { case a ~ b ~ c  => a + b + c }
-    def ip6_8 = flatOpt(h16Colonh16N(5)) ~ "::" ~ h16 ^^ { case a ~ b ~ c => a + b + c }
-    def ip6_9 = flatOpt(h16Colonh16N(6)) ~ "::" ^^ { case a ~ b => a + b }
-    def ipv6Address = (ip6_9 | ip6_8 | ip6_7 | ip6_6 | ip6_5 | ip6_4 | ip6_3 | ip6_2 | ip6_1) ^^ { IPv6AddressNode(_) }
+    def ipv6Address = IPv6Address ^^ { IPv6AddressNode(_) }
+    def ipv6Literal = "[" ~> ipv6Address <~ "]"
 
     def ipvFuturePt1 = "v" ~> rep1(hexDigit) <~ "." ^^ { _ mkString "" }
     def ipvFuturePt2 = rep1(unreserved | subDelims | ":") ^^ { _ mkString "" }
     def ipvFuture = ipvFuturePt1 ~ ipvFuturePt2 ^^ {
       case a ~ b => IPvFutureAddressNode("v" + a + "." + b)
     }
+    def ipvFutureLiteral = "[" ~> ipvFuture <~ "]"
     
-    def ipLiteral = "[" ~> (ipv6Address | ipvFuture) <~ "]"
+    def ipLiteral = ipv6Literal | ipvFutureLiteral
 
     def port = rep(digit) ^^ { l => PortNode(l.mkString.toInt) }
     def host = ipLiteral | ipv4Address | regName
     def userInfo = rep(unreserved | pctEncoded | subDelims | ":") ^^ { l => UserInfoNode(l mkString "") }
-    def authority = opt(userInfo) ~ host ~ opt(port) ^^ { case a ~ b ~ c => AuthorityNode(a, HostNode(b), c) }
+    def authority = opt(userInfo) ~ host ~ opt(port) ^^ { case a ~ b ~ c => AuthorityNode(a, b, c) }
 
     def scheme = alpha ~ (rep(alpha | digit | "+" | "-" | ".") ^^ { _ mkString "" }) ^^ { case a ~ b => a + b }
 
@@ -225,20 +237,21 @@ object Uri {
   private[rl] case class FragmentNode(value: String) extends UriPartNode
   private[rl] case class QueryStringNode(value: String) extends UriPartNode
 
-  private[rl] sealed trait IPAddress extends UriPartNode
-  private[rl] case class IPv4AddressNode(first: String, second: String, third: String, fourth: String) extends UriPartNode {
+  private[rl] sealed trait HostNodePart extends UriPartNode
+  private[rl] sealed trait IPAddress extends HostNodePart
+  private[rl] case class IPv4AddressNode(first: String, second: String, third: String, fourth: String) extends IPAddress {
     def address = {
       "%s.%s.%s.%s".format(first, second, third, fourth)
     }
   }
-  private[rl] sealed trait IPLiteralNode extends UriPartNode { def value: String }
+  private[rl] sealed trait IPLiteralNode extends IPAddress { def value: String }
   private[rl] case class IPvFutureAddressNode(value: String) extends IPLiteralNode
   private[rl] case class IPv6AddressNode(value: String) extends IPLiteralNode
 
   private[rl] case class UserInfoNode(value: String) extends UriPartNode
   private[rl] case class PortNode(value: Int) extends UriPartNode
-  private[rl] case class HostNode(value: String) extends UriPartNode
-  private[rl] case class AuthorityNode(userInfo: Option[UserInfoNode], host: HostNode, port: Option[PortNode]) extends UriPartNode
+  private[rl] case class HostNode(value: String) extends HostNodePart
+  private[rl] case class AuthorityNode(userInfo: Option[UserInfoNode], host: HostNodePart, port: Option[PortNode]) extends UriPartNode
   
 
   private def internationalize(parts: (String, String, String, String, String)) = {
