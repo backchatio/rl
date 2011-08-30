c1f4f7e9d1e1913c19bb77d77ae621fc25c1e8fe
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-08-30 20:06:41 +0100

    Adds normalization for the path, querystring and domain name

--- src/test/scala/rl/tests/DomainParserSpec.scala
+++ src/test/scala/rl/tests/DomainParserSpec.scala
@@ -5,9 +5,8 @@ import rl.DomainParser
 
 class DomainParserSpec extends Specification {
   def is =
-
-    "A domain parser should" ^
-      "reading the dat file" ^
+    "When a domain parser" ^
+      "reads the dat file, it should" ^
         "create the first level of the tree" ! { DomainParser.publicSuffixes.contains("com") must beTrue } ^
         "create the first level of the tree even when the first doesn't appear on a line on its own" ! {
           DomainParser.publicSuffixes.contains("uk") must beTrue
@@ -15,6 +14,10 @@ class DomainParserSpec extends Specification {
         "create the lower levels of the tree" ! {
           (DomainParser.publicSuffixes("jp") contains "ac" must beTrue) and
             (DomainParser.publicSuffixes("jp")("aichi") contains "*" must beTrue)
-        } ^ end
+        } ^ p ^
+      "parses a host string, the result" ^
+        "includes the tld" ! { DomainParser("backchat.io") must_== ("io", "backchat", "") } ^
+        "includes the domain" ! { DomainParser("backchat.io") must_== ("io", "backchat", "")} ^
+        "includes the subdomain" ! { DomainParser("builds.mojolly.com") must_== ("com", "mojolly", "builds") } ^ end
 
 }
